ea3dbdb733f8edd50d113307b7801b93128e840b
Vanessa Kovalsky <vanessa.kovalsky@free.fr>
2024-02-27 11:37:32 +0100

    Fix solution for form

--- corrige/06-form/src/App.tsx
+++ corrige/06-form/src/App.tsx
@@ -1,67 +1,32 @@
-import * as React from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { GameComponent, NameEditComponent, EditorEditComponent, YearEditComponent, CategoryEditComponent, GameForm} from './components';
+import GameComponent from './components/GameComponent';
+import HeaderComponent from './components/HeaderComponent';
+import LoginComponent from './components/LoginComponent';
+import GameAddComponent from './components/GameAddComponent';
+import GameListComponent from './components/GameListComponent';
+import Reservation from './DemoForm';
+import GameForm from './components/GameForm';
+import { TestFormComponent } from './components/TestFormComponent';
+import DemoMaterial from './components/DemoMaterial';
 
 function App() {
-
-  const [name, setName] = React.useState("Les aventuriers du rail");
-
-  const [editingName, setEditingName] = React.useState("Les aventuriers du rail");
-
-  // Fonction qui simule un appel asynchrone toutes les 50s
-  const loadGamename = () => {
-    setTimeout(() => {
-      setName("name from async call");
-      setEditingName("name from async call");
-    }, 50);
-  };
-
-  React.useEffect(() => {
-    loadGamename();
-  },
-  []);
-
-  const setGamenameState = () => {
-    setName(editingName);
+  const [nomDuJeu, setName] = React.useState('defaultGameName');
+  
+  const setGameNameState = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value)
   }
-
-  const [editor, setEditor] = React.useState("Days of wonders");
-
-  const setEditornameState = (newEditor: string) => {
-    setEditor(newEditor);
-  }
-
-  const [year, setYear] = React.useState(2004);
-
-  const setYearState = (newYear: number) => {
-    setYear(newYear);
-  }
-
-  const [category, setCategory] = React.useState("Familiale");
-
-  const setCategoryState = (newCategory: string) => {
-    setCategory(newCategory);
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <GameComponent gameName={name} editorName={editor} gameYear={year} category={category} />
-      <NameEditComponent 
-        initialGameName={name} 
-        editingName={editingName}
-        onNameUpdate={setGamenameState}
-        onEditingNameUpdated={setEditingName}
-      />
-      <EditorEditComponent initialEditorName={editor} onEditorUpdate={setEditornameState} />
-      <YearEditComponent initialGameYear={year} onYearUpdate={setYearState} />4
-      <CategoryEditComponent initialCategory={category} onCategoryUpdate={setCategoryState} />
-      
+      <HeaderComponent logo = {logo} />
+      {/* <LoginComponent /> */}
+      {/* <GameAddComponent nomDuJeu={nomDuJeu} onChange={setGameNameState} /> */}
+      {/* <GameComponent gameName={nomDuJeu} /> */}
+      <GameListComponent />
       <GameForm />
-
+      <TestFormComponent />
+      <DemoMaterial />
     </div>
   );
 }
---
+++ corrige/06-form/src/components/GameForm.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import { Formik, Form, Field, FormikHelpers, FormikProps} from 'formik';
+import * as Yup from 'yup';
+import {TextField} from '@mui/material';
+
+interface Values {
+    nomDuJeu: string
+    editeur: string
+    anneeDeSortie: number
+    categorie: string
+}
+const GameForm = () => {
+    return (
+        <Formik 
+            initialValues={{
+                nomDuJeu: "Nom du jeu",
+                editeur: 'Edition',
+                anneeDeSortie: 2024,
+                categorie: 'test'
+            }}
+        onSubmit= {( values: Values, {setSubmitting }: FormikHelpers<Values>) => {
+            setTimeout(() =>  {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+            }, 500);
+        }}
+        validationSchema={Yup.object().shape({
+            nomDuJeu: Yup.string()
+                .matches(
+                    /^([A-Z]*)([a-z]*)([0-9]*)\S$/
+                )
+                .required(
+                    'Merci de saisir le nom d\'un jeu'
+                )
+        })
+    }
+        >
+        {(props: FormikProps<Values>) => {
+            const {
+                values,
+                touched,
+                errors,
+                handleBlur,
+                handleChange,
+                isSubmitting
+            } = props
+            return(
+                <Form>
+                <TextField 
+                    id="nomDuJeu"
+                    name="nomDuJeu"
+                    placeholder="nomDuJeu"
+                    type="text"
+                    value={values.nomDuJeu}
+                    helperText={
+                        errors.nomDuJeu && touched.nomDuJeu
+                            ? errors.nomDuJeu
+                            : 'Entrer le nom du jeu'
+                    }
+                    error={
+                        errors.nomDuJeu && touched.nomDuJeu
+                            ? true
+                            :false
+                    }
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                <Field 
+                    id="anneeDeSortie"
+                    name="anneeDeSortie"
+                    placeholder="anneeDeSortie"
+                    type="number"
+                />
+                <button type="submit">Envoyer</button>
+            </Form>
+            )
+        }}
+           
+        </Formik>
+
+    );
+}
+
+export default GameForm
